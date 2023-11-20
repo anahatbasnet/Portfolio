ce00c70cf068c73abbf39cb3db5744c9179e86f4
Anahat <67598161+anahatbasnet@users.noreply.github.com>
2023-11-20 22:17:14 +0545

    second-commit

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,65 +1,69 @@
 import React from "react";
 import { FaGithub, FaLink, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-import { bsfillPersonLinesFill } from "react-icons/bs";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
-    const links = [
-        {
-            id:1,
-            child:(
-                <>
-                LinkedIn <FaLinkedin size={30} />
-                </>
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/anahat-basnet-7a34bb263/",
+      style: "rounder-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          Github <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/anahatbasnet",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:anahatbasnet1@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/Resume.pdf",
+      style: "rounder-br-md",
+      download: true,
+    },
+  ];
 
-            ),
-            href: "https://www.linkedin.com/in/anahat-basnet-7a34bb263/",
-            style: "rounder-tr-md",
-        },
-        {
-            id:2,
-            child:(
-                <>
-                Github <FaGithub size={30} />
-                </>
-
-            ),
-            href: "https://github.com/",
-           
-        },
-        {
-            id:3,
-            child:(
-                <>
-                LinkedIn <FaLinkedin size={30} />
-                </>
-
-            ),
-            href: "https://linkedin.com",
-            style: "rounder-tr-md",
-        },
-        {
-            id:1,
-            child:(
-                <>
-                LinkedIn <FaLinkedin size={30} />
-                </>
-
-            ),
-            href: "https://linkedin.com",
-            style: "rounder-tr-md",
-        },
-    ]
   return (
     <div className="flex flex-col top-[35%] left-0 fixed">
       <ul>
-        <li className="flex justify-between items-center w-40  h-14 px-4 ml-[-100px] bg-gray-500 hover:rounded-md duration-300 hover:ml-[-10px]">
-          <a href="" className="flex justify-between items-center w-full text-white">
-            <>
-              
-            </>
-          </a>
-        </li>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] bg-gray-500 hover:rounded-md duration-300 hover:ml-[-10px] ${
+              style || ""
+            }`}>
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank">
+              {child}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
